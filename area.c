3d05fcd50ae9044d74e8dd47edf8d9ef8f9c6740
Chris Bourke <cbourke@cse.unl.edu>
2020-06-12 21:51:32 -0500

    style cleanup

--- area.c
+++ area.c
@@ -1,11 +1,10 @@
 /**
  * Author: Chris Bourke
- *
+ * 
  * This program prompts the user to enter the
  * base and the height of a triangle and computes
  * its area using the well-known formula
- *
- * 1/2 * base * height
+ *  1/2 * base * height
  *
  */
 #include <stdlib.h>
