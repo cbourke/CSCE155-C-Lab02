7b6c24f2744fb2b48d5adf2a2e231c4e362879b6
Chris Bourke <cbourke@cse.unl.edu>
2020-08-25 15:28:16 -0500

    updated placeholder so ohter systems do not issue warnings

--- ranges.c
+++ ranges.c
@@ -19,28 +19,28 @@ int main(int argc, char **argv) {
   printf("Maximum value for unsigned char: %d.\n", UCHAR_MAX);
   printf("Minimum value for char: %d.\n", CHAR_MIN);
   printf("Maximum value for char: %d.\n", CHAR_MAX);
-  printf("Size of signed char: %d byte.\n\n", sizeof(signed char));
+  printf("Size of signed char: %ld byte.\n\n", sizeof(signed char));
 
   //integers
   printf("Data type: integer.\n");
   printf("Minimum value for signed short: %d.\n", SHRT_MIN);
   printf("Maximum value for signed short: %d. \n", SHRT_MAX);
-  printf("Size of signed short: %d bytes.\n\n", sizeof(signed short));
+  printf("Size of signed short: %ld bytes.\n\n", sizeof(signed short));
   printf("Minimum value for unsigned short: %d.\n", 0);
   printf("Maximum value for unsigned short: %d.\n", USHRT_MAX);
-  printf("Size of unsigned short: %d bytes.\n\n", sizeof(unsigned short));
+  printf("Size of unsigned short: %ld bytes.\n\n", sizeof(unsigned short));
   printf("Minimum value for signed int: %d\n", INT_MIN );
   printf("Maximum value for signed int: %d\n", INT_MAX );
-  printf("Size of signed int: %d bytes.\n\n", sizeof(signed int));
+  printf("Size of signed int: %ld bytes.\n\n", sizeof(signed int));
   printf("Minimum value for unsigned int: %u\n", 0 );
   printf("Maximum value for unsigned int: %u\n", UINT_MAX );
-  printf("Size of unsigned int: %d bytes.\n\n", sizeof(unsigned int));
+  printf("Size of unsigned int: %ld bytes.\n\n", sizeof(unsigned int));
   printf("Minimum value for signed long: %ld\n", LONG_MIN );
   printf("Maximum value for signed long: %ld\n", LONG_MAX );
-  printf("Size of signed long: %d bytes.\n\n", sizeof(signed long));
+  printf("Size of signed long: %ld bytes.\n\n", sizeof(signed long));
   printf("Minimum value for unsigned long: %lu\n", 0 );
   printf("Maximum value for unsigned long: %lu\n", ULONG_MAX );
-  printf("Size of unsigned long: %d bytes.\n\n", sizeof(unsigned long));
+  printf("Size of unsigned long: %ld bytes.\n\n", sizeof(unsigned long));
 
   //floating point types
   printf("Data type: floating-point.\n");
